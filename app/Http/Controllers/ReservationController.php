7436a753693c445b8d665e2b7f6ce5855a6d0945
yasser-aboufaris <azertyasser321@gmail.com>
2025-03-14 19:17:46 +0000

    creating he reserving functionality

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -6,7 +6,37 @@ use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    // public function takeReservation(){
-    //     $validated = 
-    // }
+    public function takeReservations(Request $request, $id)
+    {
+        $request->validate([
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ]);
+        
+        $idHouse = $id;
+        $startDate = $request->start_date;
+        $endDate = $request->end_date;
+    
+        $reservations = Reservation::where('parcking_id', $idHouse)->get();
+
+    
+        foreach ($reservations as $reservation) {
+            if (($startDate <= $reservation->end_date) && ($endDate >= $reservation->start_date)) {
+                if($i>$parking->capacity){
+                    return "parcking is full";
+                }
+                $i=$i+1;
+                return redirect()->back();
+            }
+        }
+    
+        $reservation = Reservation::create([
+            'parking_id' => $idHouse,
+            'user_id' => $userId,
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+        ]);
+    
+        return redirect()->route;
+    }
 }
